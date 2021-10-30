d28601d30cbb3660b29c9fe328743801aabf617b
Obuli Sundar R <obuliskcet@gmail.com>
2021-10-30 13:56:17 +0530

    first commit

---
+++ src/main/java/com/obuli/microservice/demo/EchoApplication.java
@@ -0,0 +1,16 @@
+package com.obuli.microservice.demo;
+
+/**
+ * @author Obuli
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoApplication.class, args);
+	}
+
+}
